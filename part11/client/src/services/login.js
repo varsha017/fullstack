4fa6274419ee9332204c78e385cfc8e8322dc18f
varsha017 <110588519+varsha017@users.noreply.github.com>
2022-10-27 12:05:28 +0530

    Delete part11 directory

--- part11/client/src/services/login.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const urlBase = '/api/login'
-const login = async (credentials) => 
-{
-  const outcome = await axios.post(urlBase, credentials)
-  return outcome.data
-}
-export default { login }
--- part11/controllers/health-check.js
+++
@@ -1,7 +0,0 @@
-const healthCheckRouter = require('express').Router()
-
-healthCheckRouter.get('/', async (request, response) => 
-{
-  response.send('ok')
-})
-module.exports =  health-healthCheckRouter
